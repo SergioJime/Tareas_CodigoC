b5c7b58f9104a3b3eaf21df222458c2c39fdd7b3
SergioJime <163787516+SergioJime@users.noreply.github.com>
2024-05-05 19:31:19 -0600

    Add files via upload

---
+++ Examen_ejercicio3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int opcion;
+	
+	
+	do{
+	
+	printf("\n------Menu Principal------\n");
+	printf("\n Ingresa un numero del 1 al 7 \n");
+	printf("\n----------------------------\n");
+	scanf("%d",&opcion);
+	
+	
+	switch(opcion){
+	case 1:
+	   printf("El dia que seleccionaste es lunes");
+	    break;
+	case 2:
+		printf("El dia que seleccionaste es Martes");
+		break;
+	case 3:
+		printf("El dia que seleccionaste es Miercoles");
+		break;
+	case 4:
+		printf("El dia que seleccionaste es Jueves");
+		break;
+	case 5:
+		printf("El dia que seleccionaste es Viernes");
+		break;
+	case 6:
+		printf("El dia que seleccionaste es Sabado");
+		break;
+	case 7:
+		printf("El dia que seleccionaste es Domingo");
+		break;
+	default:
+		printf("La opcion no es valida");
+	
+	}
+	}while(opcion != 7);
+	
+	
+	
+	
+	return 0;
+}
+
---
+++ Examen_ejercicio5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+	
+	int arreglosuma[5]= {23,4,1,44,55};
+	
+	int i, j, aux;
+		for(i=0; i < 5; i++){
+		    for(j=0; j < 5; j++){
+				if( arreglosuma[j] > arreglosuma[j+1]){
+					aux = arreglosuma[j];
+					arreglosuma[j]= arreglosuma[j+1];
+				arreglosuma[j+1+1] =aux;
+				
+				}
+			}
+	}
+	
+		printf("\n Arreglo descendente\n");
+	for(int i = 0; i < 5; i++){
+		
+	}
+
+	printf("%d", arreglosuma[i]);
+	
+	int suma;	
+	suma=0;
+		
+		for(int i=0; i < 5 ; i++){
+		suma=suma+arreglosuma[i];
+		
+	}
+	printf("%d",suma);
+
+ return 0;
+}
